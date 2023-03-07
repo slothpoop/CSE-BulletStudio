e8101bfc258805177065532151519541413863c0
slothpoop <125423406+slothpoop@users.noreply.github.com>
2023-03-07 10:17:14 -0800

    bullets (no collision to enemy)

---
+++ BulletProject101/Assets/Scripts/Game/Player/PlayerShooting.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bulletPrefab;
+
+    [SerializeField]
+    private float _bulletSpeed;
+
+    [SerializeField]
+    private Transform _gunOffset;
+
+    [SerializeField]
+    private float _timeBetweenShots;
+    
+    private bool _fireContinuously;
+    private float _lastFireTime;
+
+    void Update()
+    {
+        if (_fireContinuously)
+        {
+            float timeSinceLastFire = Time.time - _lastFireTime;
+
+            if (timeSinceLastFire >= _timeBetweenShots)
+            {
+
+            FireBullet();
+
+            _lastFireTime = Time.time;
+            }
+        }
+    }
+
+    private void FireBullet()
+    {
+        GameObject bullet = Instantiate(_bulletPrefab, _gunOffset.position, transform.rotation);
+        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = _bulletSpeed * transform.up;
+    }
+
+    private void OnFire(InputValue inputValue)
+    {
+        _fireContinuously = inputValue.isPressed;
+    }
+}
