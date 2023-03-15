971fc19dbfdffb777ec0458685d5b4a8884e8fab
slothpoop <125423406+slothpoop@users.noreply.github.com>
2023-03-15 12:59:46 -0700

    test

--- BulletProject101/Assets/Scripts/Game/Enemy1/EnemyMovement.cs
+++ BulletProject101/Assets/Scripts/Game/Enemy1/EnemyMovement.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private float _speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private float _rotationSpeed;
+
+	private Rigidbody2D _rigidbody;
+	private PlayerAwarenessController _playerAwarenessController;
+	private Vector2 _targetDirection;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_playerAwarenessController = GetComponent<PlayerAwarenessController>();
+	}
+	
+	private void FixedUpdate()
+	{
+		UpdateTargetDirction();
+		RotateTowardsTarget();
+		SetVelocity();
+	}
+
+	private void UpdateTargetDirction()
+	{
+		if (_playerAwarenessController.AwareOfPlayer)
+		{
+			_targetDirection = _playerAwarenessController.DirectionToPlayer;
+		}
+		else
+		{
+			_targetDirection = Vector2.zero;
+		}
+	}
+
+	private void RotateTowardsTarget()
+	{
+		if (_targetDirection == Vector2.zero)
+		{
+			return;
+		}
+		Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+		Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+		_rigidbody.SetRotation(rotation);
+	}
+
+	private void SetVelocity()
+	{
+		if (_targetDirection == Vector2.zero)
+		{
+			_rigidbody.velocity = Vector2.zero;
+		}
+		else
+		{
+			_rigidbody.velocity = transform.up * _speed;
+		}
+	}
 }
+
