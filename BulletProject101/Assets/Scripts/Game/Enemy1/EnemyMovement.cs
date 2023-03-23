71656a9fce44a8f344a8c4c11974360c8a074695
Anthony Acosta <125158564+Acosta1Anthony@users.noreply.github.com>
2023-03-22 17:14:58 -0700

    enemy movement

--- BulletProject101/Assets/Scripts/Game/Enemy1/EnemyMovement.cs
+++ BulletProject101/Assets/Scripts/Game/Enemy1/EnemyMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public enum states{
+        straight,
+        wavy
     }
 
-    // Update is called once per frame
+    public states currentState;
+    public float speed;
+
+    [Header("Wavy Variables")]
+    public float amplitude;
+    public float period;
+    public float shift;
+    public float yChange;
+    public float newX;
+    public float newY;
+
+    private Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        
+    switch(currentState){
+        case states.straight:
+        myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, -speed);
+        break;
+      case states.wavy:
+            newY = transform.position.y - yChange;
+            newX = amplitude * Mathf.Sin(period * newY) + shift;
+            Vector2 tempPosition = new Vector2(newX, newY);
+            transform.position = tempPosition;
+        break;
     }
+}
+
 }
