7575b46852755752c23ab88d23eeada181313923
slothpoop <125423406+slothpoop@users.noreply.github.com>
2023-04-04 11:41:52 -0700

    pablos merge

---
+++ BulletProject101/Assets/Scripts/Game/PabloScript/CreditScroll.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CreditScroll : MonoBehaviour
+{
+    float speed = 100.0f;
+    float textPosBegin = -370.0f;
+    float boundaryTextEnd = 450.0f;
+
+    RectTransform myGorectTransform;
+    [SerializeField]
+    TextMeshProUGUI mainText;
+
+    [SerializeField]
+    bool isLooping = false;
+
+    void Start()
+    {
+        myGorectTransform = gameObject.GetComponent<RectTransform>();
+        StartCoroutine(AutoScrollText());
+    }
+
+    IEnumerator AutoScrollText()
+    {
+        while(myGorectTransform.localPosition.y < boundaryTextEnd)
+        {
+            myGorectTransform.Translate(Vector3.up * speed * Time.deltaTime);
+            if(myGorectTransform.localPosition.y > boundaryTextEnd)
+            {
+                if (isLooping)
+                {
+                    myGorectTransform.localPosition = Vector3.up * textPosBegin;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            yield return null;
+        }
+    }
+}
