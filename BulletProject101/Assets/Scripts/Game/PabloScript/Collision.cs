09561332534ff66446a777b2e3817e894f1e8b55
slothpoop <125423406+slothpoop@users.noreply.github.com>
2023-04-06 00:58:54 -0700

    Revert "ignored for now test"
    This reverts commit 5b8e701dfd0a9d9d6fa23e6d4a1788e9d1b09617.

--- BulletProject101/Assets/Scripts/Game/PabloScript/Collision.cs
+++ BulletProject101/Assets/Scripts/Game/PabloScript/Collision.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class Collision : MonoBehaviour
 {
     //Health can be changed on Unity
-    public int health = 1;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar1 healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
     
    //Triggered when 2 Objects collide onto one another
     void OnTriggerEnter2D() {
         Debug.Log ("Trigger");
 
-        health--;
+        currentHealth--;
+
+        healthBar.SetHealth(currentHealth);
     }
 
     //Update when collision happens, checks if health is 0 
     void Update() {
-        if (health <= 0) {
+        if (currentHealth <= 0) {
             Die();
         }
     }
