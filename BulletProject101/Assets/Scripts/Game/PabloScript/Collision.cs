39e7640b2e6b73906751b8227e5e90ed7f227bcd
slothpoop <125423406+slothpoop@users.noreply.github.com>
2023-04-06 01:01:03 -0700

    Revert "Revert "test""
    This reverts commit 3d44d9fd64a16c6d4acf46dde8ec750a6791030b.

--- BulletProject101/Assets/Scripts/Game/PabloScript/Collision.cs
+++ BulletProject101/Assets/Scripts/Game/PabloScript/Collision.cs
@@ -5,29 +5,18 @@ using UnityEngine;
 public class Collision : MonoBehaviour
 {
     //Health can be changed on Unity
-    public int maxHealth = 100;
-    public int currentHealth;
-
-    public HealthBar1 healthBar;
-
-    void Start()
-    {
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
-    }
+    public int health = 1;
     
    //Triggered when 2 Objects collide onto one another
     void OnTriggerEnter2D() {
         Debug.Log ("Trigger");
 
-        currentHealth--;
-
-        healthBar.SetHealth(currentHealth);
+        health--;
     }
 
     //Update when collision happens, checks if health is 0 
     void Update() {
-        if (currentHealth <= 0) {
+        if (health <= 0) {
             Die();
         }
     }
