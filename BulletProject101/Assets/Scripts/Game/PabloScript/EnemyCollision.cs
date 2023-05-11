4d55f71936b0e4e13764f1164b09f5f20d58be68
ronnyramze <rsramzi97@gmail.com>
2023-05-11 03:34:55 -0700

    ?
    .

--- BulletProject101/Assets/Scripts/Game/PabloScript/EnemyCollision.cs
+++ BulletProject101/Assets/Scripts/Game/PabloScript/EnemyCollision.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class EnemyCollision : MonoBehaviour
 {
+
+
     //Health can be changed on Unity
     public int health = 1;
+    private ScoreManager scoreManager;
 
+
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
     //Triggered when 2 Objects collide onto one another
     void OnTriggerEnter2D() 
     {
+        
         Debug.Log ("Trigger");
         health--;
     }
@@ -18,13 +27,17 @@ public class EnemyCollision : MonoBehaviour
     void Update() {
         if (health <= 0) 
         {
+          
             Die();
+          
+            
         }
     }
 
     //Destroys Object
     void Die() 
     {
+        scoreManager.UpdateScore(10);
         Destroy(gameObject);
     }
 }
