b6430cb21df6246db2c7d20194e3bb8781a3194c
pablov07 <pablovazquez606@gmail.com>
2023-05-11 04:01:16 -0700

    Revert "?"
    This reverts commit 4d55f71936b0e4e13764f1164b09f5f20d58be68.

--- BulletProject101/Assets/Scripts/Game/PabloScript/EnemyCollision.cs
+++ BulletProject101/Assets/Scripts/Game/PabloScript/EnemyCollision.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class EnemyCollision : MonoBehaviour
 {
-
-
     //Health can be changed on Unity
     public int health = 1;
-    private ScoreManager scoreManager;
 
-
-    void Start()
-    {
-        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
-    }
     //Triggered when 2 Objects collide onto one another
     void OnTriggerEnter2D() 
     {
-        
         Debug.Log ("Trigger");
         health--;
     }
@@ -27,17 +18,13 @@ public class EnemyCollision : MonoBehaviour
     void Update() {
         if (health <= 0) 
         {
-          
             Die();
-          
-            
         }
     }
 
     //Destroys Object
     void Die() 
     {
-        scoreManager.UpdateScore(10);
         Destroy(gameObject);
     }
 }
