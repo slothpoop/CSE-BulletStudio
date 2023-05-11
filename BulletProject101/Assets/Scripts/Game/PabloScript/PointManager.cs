59a9f858865d05542f95312af4969fb92281b0f1
pablov07 <pablovazquez606@gmail.com>
2023-05-11 03:15:12 -0700

    Fixes

--- BulletProject101/Assets/Scripts/Game/PabloScript/PointManager.cs
+++ BulletProject101/Assets/Scripts/Game/PabloScript/PointManager.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PointManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     public int score;
+    public TMP_Text scoreText;
+
     void Start()
     {
-        
+        scoreText.text= "Score: " + score;
     }
 
-    // Update is called once per frame
     public void UpdateScore(int points)
     {
         score += points; 
+        scoreText.text= "Score: " + score;
     }
 }
